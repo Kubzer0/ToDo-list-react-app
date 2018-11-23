15510178a5a52ede6257aec8dd3af6c7ca14ca0e
kubzer0 <jakub.gryka@gmail.com>
2018-11-23 10:56:48 +0100

    complete task logic using spread

--- src/toDo/ToDo.js
+++ src/toDo/ToDo.js
@@ -29,12 +29,15 @@ class ToDo extends React.Component {
 
     completeTask = (taskKey) => this.setState({
         tasks: this.state.tasks.map(
-            task => {
-                if (task.key !== taskKey){
-                task.isCompleted = true
+            task => (
+                (task.key === taskKey) ?
+                {
+                    ...task,
+                    isCompleted: true
                 }
-                return task
-            }
+                :
+                task
+            )
         )
     })
 
